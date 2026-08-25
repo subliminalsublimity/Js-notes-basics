7932aae3efa682ab60d32562d864008474d9b705
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2026-08-25 08:22:54 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 14453
+let accountEmail = "dakshgaur10@gmail.com"
+var accountPassword = "1236"
+accountCity = "New Delhi"
+
+// accountId = 4 //not a
+accountEmail = "dgdg@gmail.com"
+accountPassword = "461"
+accountCity = "Mumbai"
+let accountState;
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+

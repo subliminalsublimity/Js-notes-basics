86427d9edfa29fcb8e96a49ce4bb7fb3fd7a40ef
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2026-09-23 07:38:12 +0000

    strings basics

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "daksh"
+const repoCount = 30
+
+//console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('daksh-fc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   daksh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://daksh.com/daksh%20gaur"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('gupta'));
+
+console.log(gameName.split('-'));
